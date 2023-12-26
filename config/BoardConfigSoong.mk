1bebb38efdbe4f037e40051b7eb42a64f3a8929f
cjh1249131356 <cjh1249131356@gmail.com>
2023-12-26 21:02:14 +0800

    soong: Add TARGET_USES_NOTHING_CAMERA

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -39,7 +39,8 @@ SOONG_CONFIG_exthmGlobalVars += \
     target_process_sdk_version_override \
     target_surfaceflinger_udfps_lib \
     uses_egl_display_array \
-    uses_oplus_camera
+    uses_oplus_camera \
+    uses_nothing_camera
 
 SOONG_CONFIG_NAMESPACES += exthmNvidiaVars
 SOONG_CONFIG_exthmNvidiaVars += \
@@ -62,6 +63,7 @@ SOONG_CONFIG_exthmGlobalVars_gralloc_handle_has_reserved_size := $(TARGET_GRALLO
 SOONG_CONFIG_exthmGlobalVars_gralloc_handle_has_ubwcp_format := $(TARGET_GRALLOC_HANDLE_HAS_UBWCP_FORMAT)
 SOONG_CONFIG_exthmGlobalVars_uses_egl_display_array := $(TARGET_USES_EGL_DISPLAY_ARRAY)
 SOONG_CONFIG_exthmGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
+SOONG_CONFIG_exthmGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
 SOONG_CONFIG_exthmNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
 SOONG_CONFIG_exthmQcomVars_qti_vibrator_use_effect_stream := $(TARGET_QTI_VIBRATOR_USE_EFFECT_STREAM)
 SOONG_CONFIG_exthmQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
