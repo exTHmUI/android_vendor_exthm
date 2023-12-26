1214c4fbfcb3bc815b12968fc37a9451d1bc5ead
Nolen Johnson <johnsonnolen@gmail.com>
2023-12-26 19:59:52 +0800

    soong: Bring bootloader_message_offset back This is squashed revert of:
        Revert "config: Remove BOOTLOADER_MESSAGE_OFFSET"
    
        This reverts commit a8bbd0e4aa41915818774984e04c977afe234498.
    
        Change-Id: I66df34e901e87e543102377469a150a9bf03e5b8
    
        Revert "Revert "soong: Export bootloader_message_offset to dependencies""
    
        This reverts commit d0b4f9894add43e5caa8720575391e781cab2cf2.
    
        Reason for revert: Bringing this back, LGE 8996 lives.
    
        Change-Id: Iae68fee53ba647d8c64c54388e7296232adf4db8

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -31,6 +31,7 @@ SOONG_CONFIG_exthmGlobalVars += \
     qti_vibrator_effect_lib \
     qti_vibrator_use_effect_stream \
     additional_gralloc_10_usage_bits \
+    bootloader_message_offset \
     gralloc_handle_has_custom_content_md_reserved_size \
     gralloc_handle_has_reserved_size \
     target_init_vendor_lib \
@@ -59,6 +60,7 @@ SOONG_CONFIG_exthmQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_
 SOONG_CONFIG_exthmQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
 
 # Set default values
+BOOTLOADER_MESSAGE_OFFSET ?= 0
 TARGET_GRALLOC_HANDLE_HAS_CUSTOM_CONTENT_MD_RESERVED_SIZE ?= false
 TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE ?= false
 TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS ?= 0
@@ -68,6 +70,7 @@ TARGET_SURFACEFLINGER_UDFPS_LIB ?= surfaceflinger_udfps_lib
 
 # Soong value variables
 SOONG_CONFIG_exthmGlobalVars_additional_gralloc_10_usage_bits := $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS)
+SOONG_CONFIG_exthmGlobalVars_bootloader_message_offset := $(BOOTLOADER_MESSAGE_OFFSET)
 SOONG_CONFIG_exthmGlobalVars_target_init_vendor_lib := $(TARGET_INIT_VENDOR_LIB)
 SOONG_CONFIG_exthmGlobalVars_target_ld_shim_libs := $(subst $(space),:,$(TARGET_LD_SHIM_LIBS))
 SOONG_CONFIG_exthmGlobalVars_target_process_sdk_version_override := $(TARGET_PROCESS_SDK_VERSION_OVERRIDE)
